2282a82a96353460bffa00fb9a2548758735489a
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2022-01-28 13:11:44 -0600

    Started calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { calculate } from './calculator';
 
 function App() {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState(0)
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault()
+    setOutput(calculate(input))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={(e: React.FormEvent<HTMLInputElement>) => setInput(e.currentTarget.value)}></input>
+      <input type='submit' onClick={handleSubmit}></input>
+      <input type='text' value={output} readOnly></input>
     </div>
   );
 }
---
+++ src/calculator.ts
@@ -0,0 +1,78 @@
+function log(message: string) {
+    const enabled = true
+    if(enabled) {
+        console.log(message)
+    }
+}
+
+export function calculate(inputString: string): number {
+    let inputArray = inputString.split(" ")
+
+    while (true) {
+        const multiplicationIndex = inputArray.indexOf("*")
+        const divisionIndex = inputArray.indexOf("/")
+        if (multiplicationIndex === -1 && divisionIndex === -1) {
+            break
+        }
+        if (multiplicationIndex < divisionIndex && multiplicationIndex !== -1) {
+            inputArray = [
+                ...inputArray.slice(0, multiplicationIndex - 1),
+                multiplicationRule(inputArray[multiplicationIndex - 1], inputArray[multiplicationIndex + 1]),
+                ...inputArray.slice(multiplicationIndex + 2)
+            ]
+        } else {
+            inputArray = [
+                ...inputArray.slice(0, divisionIndex - 1),
+                divisionRule(inputArray[divisionIndex - 1], inputArray[divisionIndex + 1]),
+                ...inputArray.slice(divisionIndex + 2)
+            ]
+        }
+    }
+
+    while (true) {
+        const additionIndex = inputArray.indexOf("+")
+        const substractionIndex = inputArray.indexOf("-")
+        if(additionIndex === -1 && substractionIndex === -1) {
+            break
+        } 
+        if (additionIndex < substractionIndex && additionIndex !== -1) {
+            inputArray = [
+                ...inputArray.slice(0, additionIndex - 1),
+                additionRule(inputArray[additionIndex - 1], inputArray[additionIndex + 1]),
+                ...inputArray.slice(additionIndex + 2)
+            ]
+        } else {
+            inputArray = [
+                ...inputArray.slice(0, substractionIndex - 1),
+                substractionRule(inputArray[substractionIndex - 1], inputArray[substractionIndex + 1]),
+                ...inputArray.slice(substractionIndex + 2)
+            ]
+        }
+    }
+
+    return parseInt(inputArray[0])
+}
+
+function additionRule(firstNumber: string, secondNumber: string): string {
+    const result = (parseInt(firstNumber) + parseInt(secondNumber)).toString()
+    log(`${firstNumber} + ${secondNumber} = ${result}`)
+    return result
+}
+
+function substractionRule(firstNumber: string, secondNumber: string): string {
+    const result = (parseInt(firstNumber) - parseInt(secondNumber)).toString()
+    log(`${firstNumber} - ${secondNumber} = ${result}`) 
+    return result
+}
+
+function multiplicationRule(firstNumber: string, secondNumber: string): string {
+    const result = (parseInt(firstNumber) * parseInt(secondNumber)).toString()
+    log(`${firstNumber} * ${secondNumber} = ${result}`) 
+    return result
+}
+
+function divisionRule(firstNumber: string, secondNumber: string): string {
+    const result = (parseInt(firstNumber) / parseInt(secondNumber)).toString()
+    log(`${firstNumber} / ${secondNumber} = ${result}`) 
+    return result
+}
